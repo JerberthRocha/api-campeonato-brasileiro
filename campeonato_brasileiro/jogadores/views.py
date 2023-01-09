f13f8b2de0ecab6f3a1d67f22d0663a1f99aae75
jerberthrocha-wsl <jerberthrs@gmail.com>
2023-01-09 18:05:52 -0400

    Criando ViewSet dos models Jogador, Time e Campeonato

--- campeonato_brasileiro/jogadores/views.py
+++ campeonato_brasileiro/jogadores/views.py
@@ -1,35 +1,20 @@
-# from rest_framework.views import APIView
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from rest_framework import viewsets
 from .models import Time, Jogador
-
-@api_view()
-def times_view(request):
-    times = Time.objects.all()
-    saida = [{
-        'time': time.nome,
-        'divisao': time.divisao_id
-    } for time in times]
-
-    return Response(saida)
-
-@api_view()
-def jogadores_view(request):
-    jogadores = Jogador.objects.all()
-    saida = [{
-        'nome': jogador.nome,
-        'foto': jogador.foto,
-        'id_time': jogador.time_id
-    }for jogador in jogadores]
-
-    return Response(saida)
-
-# class APITimesView(APIView):
-#     "Este endereço lista todos os times do campeonato brasileiro série A e B"
-#     def get(self, request):
-#         times = Time.objects.all()
-#         serializer = TimeSerializer(times, many=True)
-
-#         return Response(serializer.data)
+from .serializers import JogadorSerializer, TimeSerializer
+from django.shortcuts import render
 
 
+class JogadorViewSet(viewsets.ModelViewSet):
+    """Mostra os jogadores do Campeonato Brasileiro séries A e B."""
+    queryset = Jogador.objects.order_by('-id')
+    serializer_class = JogadorSerializer
+
+
+class TimeViewSet(viewsets.ModelViewSet):
+    """Mostra os times do Campeonato Brasileiro séries A e B."""
+    queryset = Time.objects.order_by('-id').select_related('divisao')
+    serializer_class = TimeSerializer
+
+
+def home(request):
+    return render(request, 'jogadores/pages/home.html')
