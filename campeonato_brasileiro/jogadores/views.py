f4feb60e8a96be4a25ed21335b339b96cd8ea074
jerberthrocha-wsl <jerberthrs@gmail.com>
2023-01-10 16:28:59 -0400

    Criando ElencoViewSet

--- campeonato_brasileiro/jogadores/views.py
+++ campeonato_brasileiro/jogadores/views.py
@@ -6,28 +6,32 @@ from .models import Time, Jogador
 from .serializers import JogadorSerializer, TimeSerializer, ElencoSerializer
 from django.shortcuts import render
 from rest_framework.response import Response
-
-
-class JogadorViewSet(viewsets.ModelViewSet):
-    """Mostra todos os jogadores do Campeonato Brasileiro séries A e B."""
-    # queryset = Jogador.objects.order_by('-id')
-    queryset = Jogador.objects.all()
-    serializer_class = JogadorSerializer
-
-
-@permission_classes((permissions.IsAuthenticatedOrReadOnly,))
-class ElencoViewSet(APIView):
-    """Mostra o elenco completo de cada equipe do Campeonato Brasileiro séries A e B."""
-    def get(self, request, pk):
-        elenco = Jogador.objects.filter(time=pk).select_related('time')
-        serializer = ElencoSerializer(elenco, many=True)
-        return Response(serializer.data)
     
 
 class TimeViewSet(viewsets.ModelViewSet):
     """Mostra todos as equipes que participam do Campeonato Brasileiro séries A e B."""
     queryset = Time.objects.all().select_related('divisao')
     serializer_class = TimeSerializer
+
+
+class JogadorViewSet(viewsets.ModelViewSet):
+    """Mostra todos os jogadores do Campeonato Brasileiro séries A e B."""
+    queryset = Jogador.objects.all().select_related('time')
+    serializer_class = JogadorSerializer
+
+
+@permission_classes((permissions.IsAuthenticatedOrReadOnly,))
+class ElencoViewSet(APIView):
+    """Mostra o elenco completo de cada equipe do Campeonato Brasileiro séries A e B."""
+    def get(self, request, **kwargs):
+        parametro = str(*kwargs.values())
+        if parametro.isnumeric():
+            elenco = Jogador.objects.filter(time__id__icontains=parametro).select_related('time')
+        else:
+            elenco = Jogador.objects.filter(time__nome__icontains=parametro).select_related('time')
+
+        serializer = ElencoSerializer(elenco, many=True)
+        return Response(serializer.data)
 
 
 def home(request):
