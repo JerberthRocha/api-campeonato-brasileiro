bcb6c597d948a3ed07fc0b9e68c3269d9089fce1
jerberthrocha-wsl <jerberthrs@gmail.com>
2023-01-11 12:56:12 -0400

    Configurando URL dos times de cada divisão

--- campeonato_brasileiro/jogadores/urls.py
+++ campeonato_brasileiro/jogadores/urls.py
@@ -1,16 +1,14 @@
 from rest_framework.routers import DefaultRouter
-from .views import JogadorViewSet, TimeViewSet, ElencoViewSet, DivisaoViewSet, home
+from .views import JogadorViewSet, TimeViewSet, ElencoViewSet, DivisaoViewSet, TimeDivisaoViewSet, home
 from django.urls import path
 
 router = DefaultRouter()
 router.register('jogadores', JogadorViewSet),
 router.register('times', TimeViewSet), 
-#TODO url de elenco -> /campeonato-brasileiro/elenco/<int:id> -> substitui na url o id pelo nome do clube
-# router.register('campeonato', CampeonatoViewSet), 
 
 urlpatterns = [
     path('home', home),
-    # path('elenco/<int:pk>', ElencoViewSet.as_view(), name='elenco'),
     path('elenco/<str:time>', ElencoViewSet.as_view(), name='elenco'),
     path('jogadores/<str:divisao>', DivisaoViewSet.as_view(), name='divisao'),
+    path('times/<str:campeonato>', TimeDivisaoViewSet.as_view(), name='campeonato'),
 ]
