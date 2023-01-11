528718a40bf0ebbd13991534de635f6cbd74f40f
jerberthrocha-wsl <jerberthrs@gmail.com>
2023-01-11 15:04:15 -0400

    Configurando arquivos estáticos

--- campeonato_brasileiro/campeonato_brasileiro/settings.py
+++ campeonato_brasileiro/campeonato_brasileiro/settings.py
@@ -144,6 +144,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'campeonato_brasileiro/templates/static'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
